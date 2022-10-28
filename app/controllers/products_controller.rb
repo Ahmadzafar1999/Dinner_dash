eaa3a5e786c2651a457536eaab471e0d8a54a05b
Ahmad Zafar <ahmad.zafar@devsinc.com>
2022-10-28 14:03:18 +0500

    updates and changes to Product controller and view files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,37 +18,21 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to product_url(@product), notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      redirect_to new_product_path
     end
   end
 
   def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to product_url(@product), notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
+    @product.update(product_params)
+    redirect_to product_path(@product)
   end
 
   def destroy
     @product.destroy
-
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to products_path
   end
 
   private
