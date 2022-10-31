c896b1abed1efa927d50d77b6dbb1a98ef6881e6
Ahmad Zafar <ahmad.zafar@devsinc.com>
2022-10-31 05:44:14 +0500

    add/remove items from cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,8 +3,8 @@
 # This is the line item model, a record of this model will contain all the information together
 class LineItem < ApplicationRecord
   belongs_to :product
-  belongs_to :cart
-  belongs_to :order
+  belongs_to :cart, optional: true
+  belongs_to :order, optional: true
   validates :subtotal, :quantity, :product_id, presence: true
-  validates :subtotal, :quantity, numericality: { greater_than: 0 }
+  validates :subtotal, :quantity, numericality: { greater_than_or_equal_to: 0 }
 end
