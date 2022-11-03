f3620bdbd5b2d2ea0deda25423e8e905ea81e478
Ahmad Zafar <ahmad.zafar@devsinc.com>
2022-11-03 22:10:09 +0500

    heroku :: deployment

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,6 +48,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:title, :description, :price, :image)
+    params.require(:product).permit(:title, :description, :price)
   end
 end
